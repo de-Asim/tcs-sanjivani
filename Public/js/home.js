a7edae7eb3c7c9e6fdbf78d3206eafbe1a391e24
ASIM <info.asimde01@gmail.com>
2021-11-26 14:33:49 +0530

    responsive

--- Public/js/home.js
+++ Public/js/home.js
@@ -9,23 +9,37 @@ values.forEach(element => {
 
 const submitBtn= document.getElementById('submitBtn');
 
+validateForm=()=>{
+    let taskValue = document.forms['setTaskForm']['setTaskInput'].value;
+    if(taskValue!=null && taskValue!=''){
+        return true;
+    }
+    else{
+        let warning = document.getElementById('warning');
+        warning.innerHTML=`**Task can't be empty**`;
+        return false;
+    }
+}
 
 
 submitBtn.addEventListener('click',()=>{
     let selectedEmployee = dropdown.value;
     localStorage.setItem("eName",selectedEmployee);
     const assignBox = document.getElementById('assignBox');
-    assignBox.innerHTML=`<div class="header">Please assign a task to Mr/Ms. ${selectedEmployee}</div>
-    <form name="setTaskForm" id="setTaskForm" action='/submit' method="post">
-        <textarea name="setTaskInput" id="setTaskInput" cols="" rows="4" placeholder="Describe the task here:" required></textarea>
+    assignBox.innerHTML=`<div class="header"><span id='goback'><i class="fas fa-arrow-left"></i></span>Please assign a task to Mr/Ms. ${selectedEmployee}</div>
+    <form name="setTaskForm" id="setTaskForm" action='/submit' method="post" onsubmit="return validateForm()">
+        <textarea name="setTaskInput" id="setTaskInput" cols="" rows="4" placeholder="Describe the task here:"></textarea>
+        <div id = 'warning'></div>
         <button type="submit" id="setTaskBtn" class="Btn">Assign Task</button>
     </form>`;
     const employeeForm = document.getElementById("employeeForm");
     employeeForm.style.display="none";
     assignBox.style.display="block";
+    let goback = document.getElementById('goback');
+    goback.addEventListener('click',()=>{
+        employeeForm.style.display="flex";
+        assignBox.style.display="none";
+    })
 })
-// function validateForm(){
-    
-// }
 }
 fetchData();
--- Public/js/submit.js
+++ Public/js/submit.js
@@ -1,4 +1,26 @@
+let warning=document.getElementById('warning');
 let eName = localStorage.getItem("eName");
 console.log(eName)
 const greet = document.getElementById('greet');
 greet.innerHTML=`hello ${eName} , please submit your task`
+
+validateForm = ()=>{
+    let fileValue = document.forms['submitForm']['fileInput'].value;
+    if(fileValue!=null && fileValue != ''){
+        return true;
+    }
+    else{
+        warning.innerHTML=`**Can't submit empty file** `
+        return false;
+    }
+}
+removeWarn =(e)=>{
+    let fileValue = document.forms['submitForm']['fileInput'].value;
+    if(fileValue!=null && fileValue!=''){
+        warning.innerHTML=``;
+        let fileName = document.getElementById('filename');
+        let newName = e.replace(/^.*\\/,"");
+        fileName.innerHTML=`${newName}`;
+        fileName.style.color='white';
+    }
+}
